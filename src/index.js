254f20395fdf0ac50392425ee947ff1e26a942c2
gliatem <gliatem@gmail.com>
2022-12-28 16:10:45 +0100

    refactor y tratmiento de errores

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+import { InvalidInputError } from '#Errors/invalidInputError';
+import { getOperator } from '#Lib/getOperator.js';
 import { promptQuestion } from '#Lib/promptQuestion.js';
 
 (async () => {
-    // 1º Capturar la entrada del usuario por consola
-    const userAnswer = await promptQuestion('Introduce tu operacion: ');
-    console.log(userAnswer);
+    try {
+        // 1º Capturar la entrada del usuario por consola
+        const userAnswer = await promptQuestion('Introduce tu operacion: ');
 
-    // 2º Validar la entrada y separar las partes de la misma en operandos y operador
+        // 2º Validar la entrada y separar las partes de la misma en operandos y operador
+        const standarizeInput = userAnswer.trim();
+        if (standarizeInput === '') throw new InvalidInputError();
+        const operator = getOperator(standarizeInput);
+
+        if (!operator) throw new InvalidInputError();
+    } catch (error) {
+        if (error instanceof InvalidInputError) console.log(error.message);
+        else
+            console.log(
+                `Error no controlado: ${error.message}. Stack: ${error.stack}`
+            );
+    }
     // 3º Realizar la operacion
     // 4º Mostrar resultado por consola
 })();
---
+++ src/lib/getOperator.js
@@ -0,0 +1,18 @@
+import { OPERATORS } from '#Constants/operators';
+import { InvalidInputError } from '#Errors/invalidInputError';
+
+export const getOperator = (standarizeInput) => {
+    let operator;
+    for (const allowedOperator of OPERATORS) {
+        if (standarizeInput.includes(allowedOperator)) {
+            if (
+                operator ||
+                standarizeInput.indexOf(allowedOperator) !==
+                    standarizeInput.lastIndexOf(allowedOperator)
+            )
+                throw new InvalidInputError();
+            operator = allowedOperator;
+        }
+    }
+    return operator;
+};
